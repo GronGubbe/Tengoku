249d21b3ebfbd04c145cf9c8083c27442c0056a0
Gröngubbe <William_w@bahnhof.se>
2026-08-08 01:37:00 +0200

    refactor: remove logging from RenderQueue

--- client/src/main/java/net/grongubbe/tengoku/client/render/queue/RenderQueue.java
+++ client/src/main/java/net/grongubbe/tengoku/client/render/queue/RenderQueue.java
@@ -1,8 +1,6 @@
 package net.grongubbe.tengoku.client.render.queue;
 
 import net.grongubbe.tengoku.client.render.frame.DrawCommand;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -10,8 +8,6 @@ import java.util.List;
 import java.util.Objects;
 
 public final class RenderQueue {
-    private final static Logger LOGGER = LogManager.getLogger(RenderQueue.class);
-
     private final List<DrawCommand> commands = new ArrayList<>();
 
     private boolean sorted;
@@ -27,24 +23,7 @@ public final class RenderQueue {
             sorted = true;
         }
 
-        LOGGER.info("RenderQueue commands:\n{}", getLogMessage(commands));
-
         return List.copyOf(commands);
-    }
-
-    private String getLogMessage(List<DrawCommand> commands) {
-        StringBuilder sb = new StringBuilder();
-        for (DrawCommand command : commands) {
-            sb.append(getCommandInfo(command)).append("\n");
-        }
-        return sb.toString();
-    }
-
-    private String getCommandInfo(DrawCommand command) {
-        return String.format("Shader ID: %d, Material ID: %d, Mesh ID: %d",
-                command.material().shader().id(),
-                command.material().id(),
-                command.mesh().id());
     }
 
     private void sort() {
